dc384af2902183458c19cf815a4ab5a3df408dab
olofahren <olof.murgarden@gmail.com>
2025-03-19 08:52:51 +0100

    tried to fix compilation and dependency issues

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import { CustomComponents, DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -60,13 +60,13 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        IconLeft: ({ className, ...props }: { className?: string;[key: string]: any }) => (
           <ChevronLeft className={cn("size-4", className)} {...props} />
         ),
-        IconRight: ({ className, ...props }) => (
+        IconRight: ({ className, ...props }: { className?: string;[key: string]: any }) => (
           <ChevronRight className={cn("size-4", className)} {...props} />
         ),
-      }}
+      } as Partial<CustomComponents>}
       {...props}
     />
   )
